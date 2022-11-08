2fdcc88b2cdf86703bcb217ee46757007d98f046
Mohan Angappan <88267976+MohanAngappan@users.noreply.github.com>
2022-11-08 23:21:18 +0530

    Add files via upload

---
+++ Assignments/Assignment-2/Karthi S/karthi.py
@@ -0,0 +1,10 @@
+import random
+import time
+while(1):
+    
+    a=random.randint(10,99)
+
+    if(a>75):
+
+        print("high temprature and humidity of:",a,"%","alarm is on")
+        time.sleep(1)
---
+++ Assignments/Assignment-2/Saravanan R/saravanan code.py
@@ -0,0 +1,7 @@
+import random
+import time
+while(1):
+    a=random.randint(10,99)
+    if (a>90):
+        print("high tempurature and humikdity of:",a,"%","Alarm on")
+        time.sleep(1)
